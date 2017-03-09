164813b39facedc52d1db5af2ee44368b0070f79
Camilo B. Moreira <camilo.moreira@fatec.sp.gov.br>
2017-03-09 20:35:33 -0300

    Update crack.c

--- CS50-2017/Week_02/pset2/crack/crack.c
+++ CS50-2017/Week_02/pset2/crack/crack.c
@@ -12,6 +12,7 @@
 */
 
 
+
 int main(int argc, char *argv[])
 {
     if (argc != 2) //Checks if number of command-line arguments is valid
@@ -29,9 +30,9 @@ int main(int argc, char *argv[])
     
     
     //All possible characters used in a DES-based hashed password (taken from gnu library)
-    const char *const seedchars = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
+    const char *const seedchars = " ./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; 
     
-    char text[9] = "\0"; //Text that is gonna be tried with crypt()
+    char text[5] = "\0"; //Text that is gonna be tried with crypt()
     
     
     /* From innermost foor-loop out.
@@ -39,103 +40,57 @@ int main(int argc, char *argv[])
      * from 1 up to 8 characteres.
      */
      
-    for (int h = 0; h <= 64 ; h++) //To check for passwords of up to 8 characters
-    {
-        if(h > 0)
-        {
-            text[8] = '\0'; //Defining null-terminator at index 8    
-            text[7] = seedchars[h]; //Iterates through the seedchars list at index 7
-        }
-                
-        for (int g = 0; g <= 64 ; g++) //To check for passwords of up to 7 characters
-        {
-            if(g > 0)
-            {
-                if (h == 0)
-                {
-                    text[7] = '\0'; //Defining null-terminator at index 7    
-                }
-                text[6] = seedchars[g]; //Iterates through the seedchars list at index 6
-            }
-                
-            for (int f = 0; f <= 64 ; f++) //To check for passwords of up to 6 characters
-            {
-                if(f > 0)
-                {
-                    if (g == 0 && h == 0)
-                    {
-                        text[6] = '\0'; //Defining null-terminator at index 6    
-                    }
-                    text[5] = seedchars[f]; //Iterates through the seedchars list at index 5
-                }
-                
-                for (int e = 0; e <= 64 ; e++) //To check for passwords of up to 5 characters
-                {
-                    if(e > 0)
-                    {
-                        if (f == 0 && g == 0 && h == 0)
-                        {
-                            text[5] = '\0'; //Defining null-terminator at index 5    
-                        }
-                        text[4] = seedchars[e]; //Iterates through the seedchars list at index 4
                         
-                        for (int d = 0; d <= 64 ; d++) //To check for passwords of up to 4 characters
-                        {
-                            if(d > 0)
-                            {
-                                if (e == 0 && f == 0 && g == 0 && h == 0)
-                                {
-                                    text[4] = '\0'; //Defining null-terminator at index 4    
-                                }
-                                text[3] = seedchars[d]; //Iterates through the seedchars list at index 3
-                            }
-                            
-                            for (int c = 0; c <= 64 ; c++) //To check for passwords of up to 3 characters
-                            {
-                                if(c > 0) 
-                                {
-                                    if (d == 0 && e == 0 && f == 0 && g == 0 && h == 0)
-                                    {
-                                        text[3] = '\0'; //Defining null-terminator at index 3
-                                    }
-                                    text[2] = seedchars[c]; //Iterates through the seedchars list at index 2
-                                }
-                                
-                                for (int b = 0; b <= 64 ; b++) //To check for passwords of up to 2 characters
-                                {
-                                    if(b > 0)
-                                    {
-                                        if (c == 0 && d == 0 && e == 0 && f == 0 && g == 0 && h == 0) 
-                                        {
-                                            text[2] = '\0'; //Defining null-terminator at index 2
-                                        }
-                                        text[1] = seedchars[b]; //Iterates through the seedchars list at index 1
-                                    }
-                                    
-                                    for (int a = 0; a <= 64 ; a++) //To check for passwords of up to 1 character
-                                    {
-                                        if(b == 0 && c == 0 && d == 0 && e == 0 && f == 0 && g == 0 && h == 0)
-                                        {
-                                            text[1] = '\0'; //Defining null-terminator at index 1
-                                        }
-                                        
-                                        text[0] = seedchars[a]; //Iterates through the seedchars list at index 0
-                                        
-                                        char *password = crypt(text, salt); //Hash var text and save it to var password
-                                        
-                                        if (strcmp(hash, password) == 0)  //Compares the hash passed as argv with created above
-                                        {
-                                            printf("%s\n", text); //prints the text that led to said hash
-                                            return 0; //Returns 0 (okay)
-                                        }
-                                    }   
-                                }   
-                            }
-                        }
-                    }
+    for (int d = 0; d <= 64 ; d++) //To check for passwords of up to 4 characters
+    {
+        if(d > 0)
+        {
+            text[4] = '\0'; //Defining null-terminator at index 4    
+            text[3] = seedchars[d]; //Iterates through the seedchars list at index 3
+        }
+        
+        for (int c = 0; c <= 64 ; c++) //To check for passwords of up to 3 characters
+        {
+            if(c > 0) 
+            {
+                if (d == 0)
+                {
+                    text[3] = '\0'; //Defining null-terminator at index 3
                 }
+                text[2] = seedchars[c]; //Iterates through the seedchars list at index 2
             }
+            
+            for (int b = 0; b <= 64 ; b++) //To check for passwords of up to 2 characters
+            {
+                if(b > 0)
+                {
+                    if (c == 0 && d == 0)
+                    {
+                        text[2] = '\0'; //Defining null-terminator at index 2
+                    }
+                    text[1] = seedchars[b]; //Iterates through the seedchars list at index 1
+                }
+                
+                for (int a = 0; a <= 64 ; a++) //To check for passwords of up to 1 character
+                {
+                    if(b == 0 && c == 0 && d == 0)
+                    {
+                        text[1] = '\0'; //Defining null-terminator at index 1
+                    }
+                    
+                    text[0] = seedchars[a]; //Iterates through the seedchars list at index 0
+                    
+                    char *password = crypt(text, salt); //Hash var text and save it to var password
+                    
+                    if (strcmp(hash, password) == 0)  //Compares the hash passed as argv with created above
+                    {
+                        printf("%s\n", text); //prints the text that led to said hash
+                        return 0; //Returns 0 (okay)
+                    }
+                }   
+            }   
         }
     }
+                    
     return 1; //Retuns 1 (error)
 }
